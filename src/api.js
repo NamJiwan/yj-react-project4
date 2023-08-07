1af81be3e4cf2e17d2601f62fce932eca5f024b1
NamJiwan <skawldhks@naver.com>
2023-08-07 13:32:37 +0900

    delete

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 
-const BASE_PATH= process.env.NODE_ENV === "production" ? "https://port-0-yj-react-project4-back-3prof2lll098hrb.sel4.cloudtype.app/" : "http://localhost:8080";
+const BASE_PATH= process.env.NODE_ENV === "production" ? "https://port-0-yj-react-project4-back-3prof2lll098hrb.sel4.cloudtype.app" : "http://localhost:8080";
 export async function rentalNotice(){
     return await fetch(`${BASE_PATH}/api/rental/notice`, {
         method: "GET",
